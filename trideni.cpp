db280c19c50b9193c6efdd763253076e34bd3203
dohnaja9 <71826546+dohnaja9@users.noreply.github.com>
2020-10-19 15:40:18 +0200

    Add files via upload

---
+++ trideni.cpp
@@ -0,0 +1,23 @@
+#include "trideni.h"
+
+using namespace std;
+
+void nacti (string nazev, seznam_slov & jmena)
+{
+
+}
+
+void serad(seznam_slov & jmena)
+{
+	
+}
+
+bool zeptej_se_jestli_vypsat()
+{
+	
+}
+
+void vypis(seznam_slov jmena)
+{
+	
+}
