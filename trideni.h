32d9d588fd5d8d833b88c66e5cd0c2df4e8b89e2
dohnaja9 <71826546+dohnaja9@users.noreply.github.com>
2020-10-22 14:39:18 +0200

    Update trideni.h

--- trideni.h
+++ trideni.h
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-typedef vector<string> seznam_slov
+typedef vector<string> seznam_slov;
 
-void nacti (string nazev, seznam_slov & jmena)
-void serad(seznam_slov & jmena)
-bool zeptej_se_jestli_vypsat()
-void vypis(seznam_slov jmena)
+void nacti (string nazev, seznam_slov & jmena);
+void serad(seznam_slov & jmena);
+bool zeptej_se_jestli_vypsat();
+void vypis(seznam_slov const& jmena);
